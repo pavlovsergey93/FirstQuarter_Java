5c2d89cf1668ffd476c1c8dcf7cc4c787b787ab0
pavlovsergey93 <pavlovsv93@gmail.com>
2021-11-07 16:19:55 +0300

    Done homework lesson 13

--- src/main/java/com/geekbrains/java/lesson13/MainClass.java
+++ src/main/java/com/geekbrains/java/lesson13/MainClass.java
@@ -20,7 +20,7 @@ public class MainClass {
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 100));
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
@@ -62,6 +62,13 @@ class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if(MainClass.cdlFinish.getCount() == CARS_COUNT-1){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         System.out.println(this.getName() + " >>>>> ФИНИШИРОВАЛ!!!!");
         MainClass.cdlFinish.countDown();
         if(MainClass.cdlFinish.getCount() == (CARS_COUNT - 1)){
@@ -88,6 +95,7 @@ class Road extends Stage {
             System.out.println(c.getName() + " начал этап: " + description);
             Thread.sleep(length / c.getSpeed() * 1000);
             System.out.println(c.getName() + " закончил этап: " + description);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
